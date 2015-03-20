cdbeb5081aa9c9a642c0716d9efe537008926b09
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-03-19 22:21:33 -0300

    Using a Constant Lookup

---
+++ testUsingEasySell.rb
@@ -0,0 +1,9 @@
+require File.expand_path("lib/virtual_store")
+
+# accessing class Set from namespace EasySell
+
+set_of_books = EasySell::Set.new
+
+set_of_books.add Book.new "TDD", "Mauricio Aniche", "123454", 247, :testes
+
+p set_of_books
