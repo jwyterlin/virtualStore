7853b0084d9e162e6b836bdf11db2d71cce97b94
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-03-26 20:59:44 -0300

    New definitions and methods to the attributes of magazine

--- lib/magazine.rb
+++ lib/magazine.rb
@@ -1,15 +1,23 @@
 class Magazine
 
-  attr_reader :title
-
   @id = 0
-
-  def initialize(title)
-    @title = title
-  end
 
   def self.id
     @id += 1
+  end
+
+  def initialize(title)
+    @id = self.class.id
+    @title = title
+  end
+
+  def id
+    @id
+  end
+
+  def title
+    title_uppercase = @title.upcase
+    "Title: #{title_uppercase}"
   end
 
 end
