400dcae9c88f62bb36de55d6826f796907448fbb
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-03-28 16:43:57 -0300

    Created two methods in DVD class using metaprogramming

--- lib/dvd.rb
+++ lib/dvd.rb
@@ -5,6 +5,19 @@ class DVD < Media
 
   attr_reader :title
 
+  def self.format_currency(*variables_and_methods)
+
+    variables_and_methods.each do |name|
+      define_method("formatted_#{name}") do
+        value = respond_to?(name) ? send(name) : instance_variable_get("@#{name}")
+        "R$ #{value}"
+      end
+    end
+
+  end
+
+  format_currency :value_with_discount, :value
+
   def initialize( title, value, category )
     super()
     @title = title
