123cd97815e3cc53ca543612fdbd8c0f484fa38c
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-03-30 23:46:01 -0300

    Created save method in Magazine class

--- lib/magazine.rb
+++ lib/magazine.rb
@@ -8,7 +8,17 @@ class Magazine
     @id = self.class.next_id
   end
 
+  def save
+    File.open("db/magazines/#{@id}.yml","w") do |file|
+      file.puts serialize
+    end
+  end
+
   private
+
+  def serialize
+    YAML.dump self
+  end
 
   def self.next_id
     Dir.glob("db/magazines/*.yml").size + 1
