5ff9c7b5c5cbab9bd783bc65b238701ea961f769
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-03-31 12:16:36 -0300

    Printing a local variable to show the exception

--- testDocumentNotFound.rb
+++ testDocumentNotFound.rb
@@ -2,6 +2,6 @@ require File.expand_path("lib/virtual_store")
 
 begin
   Magazine.find 42
-rescue
-  p "The object you're looking for was not found"
+rescue DocumentNotFound => error
+  p error
 end
